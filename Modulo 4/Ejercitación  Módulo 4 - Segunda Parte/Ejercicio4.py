fec449065c21e0d2e637665ec8306dd797215075
Tomy Suarez <tomy.suarez@estudiantes.utec.edu.uy>
2022-05-14 19:43:47 -0300

    se actualiza ej 4

--- Modulo 4/Ejercitación  Módulo 4 - Segunda Parte/Ejercicio4.py
+++ Modulo 4/Ejercitación  Módulo 4 - Segunda Parte/Ejercicio4.py
@@ -1,6 +1,8 @@
 from cmath import inf
-from numpy import NaN, Inf, arange, isscalar, asarray, array
+from operator import le
+from numpy import NaN, Inf, arange, array
 import matplotlib.pyplot as plt
+from utils import graficar
 class BasicPlethy():
     """Doc acá..."""
     
@@ -13,7 +15,7 @@ class BasicPlethy():
         
     def duration(self):
         """Returna la duración de la señal"""
-        return f"tiene {round(self.Duracion,0)} segundos" 
+        return f"{round(self.Duracion,0)} seg" 
         
     def piquitos(self)->list:
         """Obtiene los momentos en que ocurren los picos en la señal, codigo basado en deteccion de picos
@@ -23,54 +25,56 @@ class BasicPlethy():
         max= -Inf #floating point representation of (positive) infinity, sacado de numpy
         min= Inf #floating point representation of (positive) infinity, sacado de numpy
         maxposit = NaN #floating point representation of Not a Number (NaN), sacado de numpy
-        minposit = NaN #floating point representation of Not a Number (NaN), sacado de numpy
         buscar= None
-        umbral= .75 #valor para comparar con el anterior maximo
+        umbral= .75 #valor para comparar con el anterior maximo y descartar diastole en % 0.75 = 75%
         for i in arange(len(self.señal)):
             señal = self.señal[i]
             if señal > max: #este if sirve para tomar el valor maximo y guardarlo en max y maxposit toma el valor de la copia x[i]
                 max = señal
                 maxposit = x[i]
-            elif señal < min:#este if sirve para tomar el valor minimo y guardarlo en min y minposit toma el valor de la copia x[i]
+            elif señal < min:
+                """Este elif sirve para tomar los minimos y despue usarlos para compararlos con los maxios"""
                 min = señal
-                minposit = x[i]
-                
             if buscar:
                 if señal < (max*umbral):
                     """este if sirve para ir guardando en picosmax los picos maximos"""
                     picosmax.append((maxposit))
                     min = señal
-                    minposit = x[i]
                     buscar = False
             else:
                 if señal > (min*umbral):
-                    """este if sirve para retroalimentar los picos max y al for"""
+                    """este if sirve para retroalimentar los picos max y descartar los minimos al umbral"""
                     max = señal
                     maxposit = x[i]
                     buscar = True
+
+        return picosmax
+
+    def momentos(self):   
+        """"devuelve los picos pasados a segundos"""
         qiqi=[]
-        for j in picosmax:
+        for j in self.piquitos():
             """con este for tomo los valores de los picos y los paso a segundos"""
             air= round((j/self.fm),1)
             qiqi.append(air)
-
         return qiqi
+        
 
     def freq(self)->float:
-        """Calcula la frecuencia cardíaca en latidos por minutos (Heart Rate)..."""
-        return (len(self.piquitos()))/(self.Duracion/60)
+        """Calcula la frecuencia cardíaca en latidos por minutos (Heart Rate) en los primeros 10 seg"""
+        return round((len(self.piquitos()*60)/(self.Duracion)),1)
 
-        
-        
+
 lePlesti = open("plethysmography.txt", "r")
 leListe = [float(valor) for valor in lePlesti.read().split()]
-lista2 = leListe[:8000]
+lista2 = leListe[:7500]
 leMandar= BasicPlethy(leListe)
+
+print(f"los picos ocurren en los segundos: {leMandar.momentos()}")
+print(f"tiene: {len(leMandar.momentos())} picos ")
 print(f"la señal tiene un tiempo de: {leMandar.duration()}")
-print(f"tiene una frecuencia cardiaca de:{leMandar.freq()}" )
-print(f"los picos ocurren en los segundos: {leMandar.piquitos()}")
-print(f"tiene: {len(leMandar.piquitos())} picos ")
-#plt.scatter(array(leMandar.piquitos())[:,0],array(leMandar.piquitos())[:,1], color='r')
-#plt.plot(leListe, color = "g")
-#plt.show()
+print(f"tiene una frecuencia cardiaca de:{leMandar.freq()} LPM" )
+# plt.plot(lista2)
+# plt.show()
+
 
